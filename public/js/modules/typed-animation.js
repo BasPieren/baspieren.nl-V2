da9c81c5ce2424d3e694871b458a96160dacc241
Bas Pieren <bas.pieren@quicknet.nl>
2019-07-02 19:49:25 +0200

    💬 updated copy

--- public/js/modules/typed-animation.js
+++ public/js/modules/typed-animation.js
@@ -2,7 +2,7 @@ function home() {
 	const homeHeading = document.getElementsByClassName("section__heading")
 
 	const options = {
-		strings: [`Hello I'm <span class="copy--red">Bas</span> and I am a ...`],
+		strings: [`Hello I'm <span class="copy--red">Bas</span> and I'm a ...`],
 		typeSpeed: 50,
 		startDelay: 500,
 		showCursor: false
