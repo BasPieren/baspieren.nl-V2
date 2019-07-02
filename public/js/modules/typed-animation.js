6c87851e62fa822f896353533f7e82f42bf050e6
Bas Pieren <bas.pieren@quicknet.nl>
2019-07-02 17:54:29 +0200

    ✨ added home page with animation

---
+++ public/js/modules/typed-animation.js
@@ -0,0 +1,14 @@
+function home() {
+	const homeHeading = document.getElementsByClassName("section__heading")
+
+	const options = {
+		strings: [`Hello I'm <span class="copy--red">Bas</span> and I am a ...`],
+		typeSpeed: 50,
+		startDelay: 500,
+		showCursor: false
+	}
+
+	const typed = new Typed(homeHeading[0], options)
+}
+
+export { home }
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,8 @@
 "use strict"
 
 import * as header from "./modules/header/header.js"
+import * as typed from "./modules/typed-animation.js"
 
+typed.home()
 header.mobileNav()
 header.backgroundScroll()
