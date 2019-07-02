eec57c5c3be0a00e7fd47e595f9f51036ba8ca5e
Bas Pieren <bas.pieren@quicknet.nl>
2019-07-02 17:04:46 +0200

    ✨ added logo

--- public/js/modules/header/header.js
+++ public/js/modules/header/header.js
@@ -2,12 +2,14 @@ import * as helper from "../helper.js"
 
 function mobileNav() {
 	const body = document.querySelector("body"),
+		headerLogo = document.getElementsByClassName("header__logo"),
 		headerButton = document.getElementsByClassName("header__button"),
 		headerButtonIcon = document.querySelector(".header__button i"),
 		mobileNav = document.getElementsByClassName("mobileNav")
 
 	const arr = [
 		{ element: body, class: "overflow--hidden" },
+		{ element: headerLogo[0], class: "header__logo--white" },
 		{ element: headerButtonIcon, class: "header__button__icon--white" },
 		{ element: mobileNav[0], class: "mobileNav--show" }
 	]
