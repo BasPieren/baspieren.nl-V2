6593b324f3f59451c96329848d035d269f5ed10a
Bas Pieren <bas.pieren@quicknet.nl>
2019-07-02 16:51:12 +0200

    ✨ created mobile and desktop nav

---
+++ public/js/modules/header/header.js
@@ -0,0 +1,35 @@
+import * as helper from "../helper.js"
+
+function mobileNav() {
+	const body = document.querySelector("body"),
+		headerButton = document.getElementsByClassName("header__button"),
+		headerButtonIcon = document.querySelector(".header__button i"),
+		mobileNav = document.getElementsByClassName("mobileNav")
+
+	const arr = [
+		{ element: body, class: "overflow--hidden" },
+		{ element: headerButtonIcon, class: "header__button__icon--white" },
+		{ element: mobileNav[0], class: "mobileNav--show" }
+	]
+
+	helper.classToggleClick(headerButton[0], arr)
+}
+
+function backgroundScroll() {
+	const scrollPos = 0,
+		header = document.getElementsByClassName("header")
+
+	window.addEventListener("scroll", () => {
+		let windowYOffset = window.scrollY
+
+		header[0].classList.remove("animation--topSlideIn")
+
+		if (windowYOffset > scrollPos + 5) {
+			header[0].classList.add("header--hide")
+		} else if (windowYOffset === scrollPos) {
+			header[0].classList.remove("header--hide")
+		}
+	})
+}
+
+export { mobileNav, backgroundScroll }
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,6 @@
-console.log("Hello World!")
+"use strict"
+
+import * as header from "./modules/header/header.js"
+
+header.mobileNav()
+header.backgroundScroll()
